b43f863266075292e4dc194bd919a5745608d980
Andrew Au <iamandrewau@gmail.com>
2020-10-08 03:06:33 -0700

    add xinput config for kensington slimblade

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -16,3 +16,12 @@ sudo fc-cache -f -v
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 sed -i '' 's#ZSH_THEME=".*#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc
+
+# Resets middle click button on Kensington Slimblade on unlock
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
+  while read x; do
+    case "$x" in
+      *"boolean false"*) xinput set-button-map "Kensington Kensington Slimblade Trackball" 1 2 3 4 5 6 7 2;;
+    esac
+  done &
+
